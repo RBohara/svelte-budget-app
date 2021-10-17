c6ce4aa189d20d3991041172b1c783bfc863e98e
Rahul <rahul.bohara2016@gmail.com>
2021-10-17 10:47:34 +1000

    first commit

---
+++ src/ExpensesList.svelte
@@ -0,0 +1,21 @@
+<script>
+  import SectionTitle from "./Title.svelte";
+  import Expense from "./Expense.svelte";
+  import { fly } from "svelte/transition";
+  import { flip } from "svelte/animate";
+  // props
+  export let expenses = [];
+</script>
+
+<section>
+  <SectionTitle title="expenses list" />
+  <ul>
+    {#each expenses as expense, index (expense.id)}
+      <div transition:fly={{ x: 200, delay: (index + 1) * 700 }} animate:flip>
+        <Expense {...expense} />
+      </div>
+    {:else}
+      <h2>no expenses added to the list</h2>
+    {/each}
+  </ul>
+</section>
